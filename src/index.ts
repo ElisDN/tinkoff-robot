80c374a8874dbf3a46f5ee36fa677bd98bd1b0a8
ElisDN <mail@elisdn.ru>
2022-05-15 13:04:35 +0300

    Add express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello!')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello!')
+})
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080')
+})
