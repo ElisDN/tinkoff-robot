1e09d00308651d0d16fd209bc8dd255fbf256a8c
ElisDN <mail@elisdn.ru>
2022-05-15 16:25:25 +0300

    Add frontend linters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react'
 
 function App() {
   const [content, setContent] = useState(null)
 
   useEffect(() => {
     fetch('http://localhost:3001')
-        .then((response) => response.json())
-        .then((data) => {
-          setContent(data)
-        })
+      .then((response) => response.json())
+      .then((data) => {
+        setContent(data)
+      })
   }, [])
 
-  return (
-    <div>
-      {JSON.stringify(content)}
-    </div>
-  );
+  return <div>{JSON.stringify(content)}</div>
 }
 
-export default App;
+export default App
